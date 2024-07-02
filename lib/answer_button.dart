1d1c7d1be4ced2ddd5f0a5f76dc4ee1586df3e78
Erick Mutwiri <e.mutwiri@amplifytrading.com>
2024-07-02 22:11:19 +0300

    answer button styles

--- lib/answer_button.dart
+++ lib/answer_button.dart
@@ -8,10 +8,20 @@ class AnswerButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: onTap,
-      style: ElevatedButton.styleFrom(),
-      child: Text(answerText),
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 5),
+      child: ElevatedButton(
+        onPressed: onTap,
+        style: ElevatedButton.styleFrom(
+          backgroundColor: const Color.fromARGB(255, 33, 1, 95),
+          foregroundColor: Colors.white,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(40)
+          ),
+          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 40),
+        ),
+        child: Text(answerText),
+      ),
     );
   }
 }
